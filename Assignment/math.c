9addcde86eb1ad01f7d786c08c1fbf41abc65d50
adamss4 <31705041+adamss4@users.noreply.github.com>
2017-09-13 17:29:03 -0400

    Add files via upload

---
+++ Assignment/math.c
@@ -0,0 +1,18 @@
+
+#include "math.h"
+
+int math(int num1, int num2, char Operator){
+	
+	if(Operator == '+')return num1 + num2;
+	if(Operator == '-')return num1 - num2;
+	if(Operator == '/')return num1 / num2;
+	if(Operator == '*')return num1 * num2;
+	if(Operator == '%')return num1 % num2;
+
+}
+
+
+	
+	
+	
+
